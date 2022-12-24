222f167a8276ed38a14da6cf58ef9ab896a85dd6
Divyanshu Vyas <65007294+FOdivyanshh@users.noreply.github.com>
2022-12-24 12:14:51 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
-
+  
+  const array = str.split('')
+  
+  const revArr = array.reverse()
+  
+  const revString = revArr.join('')
+  
+  if(str==revString){
+    return true
+  }
+  
+  else{ 
+    return false
+  }
 }
 module.exports = palindrome
